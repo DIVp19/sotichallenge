2e31184d05aabdfe042b3934d3f1374a50334835
Prakhar Dev <prakhar.dev@soti.net>
2025-09-12 00:42:35 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('sotichallenge');
+  protected readonly title = signal('second-project');
 }
